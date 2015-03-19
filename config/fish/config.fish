4ace0098a9ded939a050aa6cabeaac6412bd6644
Dimitar Haralanov <dharalanov@me.com>
2015-03-19 09:49:29 +0000

    Remove legacy commands

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -18,25 +18,14 @@ set -x BROWSER open
 set -x LANG en_US.UTF-8
 set -x LC_CTYPE en_US.UTF-8
 
-# JRuby flags
-set -x JRUBY_OPTS '--dev --2.0 -J-Xmx2048m'
-
 # Aliases
 alias g   'git';                     complete_like g   'git'
 alias ll  'ls -laG';                 complete_like ll  'ls -laG'
 alias gg  'git status';              complete_like gg  'git status'
 alias be  'bundle exec';             complete_like be  'bundle exec'
-alias cuc 'bundle exec cucumber -c'; complete_like cuc 'bundle exec cucumber -c'
 alias tl  'tmux list-sessions';      complete_like tl  'tmux list-sessions'
 alias ta  'tmux -2 attach -t $1';    complete_like ta  'tmux -2 attach -t $1'
 alias tk  'tmux kill-session -t $1'; complete_like tk  'tmux kill-session -t $1'
-
-# Secret test helper
-function test_env
-  set -gx CUCUMBER_SUFFIX _cucumber
-  set -gx RAILS_ENV test
-  set -gx RACK_ENV test
-end
 
 # Disable tab titles
 function fish_title; end
