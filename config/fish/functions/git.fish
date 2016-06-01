98abe47cf290d503c60d90b7e6efbddbdfe89ef8
Dimitar Haralanov <dharalanov@me.com>
2016-06-01 10:29:35 +0300

    Drop redundant git branch completion for fish

--- config/fish/functions/git.fish
+++
@@ -1,4 +0,0 @@
-# Autocomplete the tags and branches as commands. The git function lets you do
-# that and having completion for it is pretty cool.
-complete -f -c git -a '(__fish_git_branches)' --description 'Branch'
-complete -f -c git -a '(__fish_git_tags)' --description 'Tag'
