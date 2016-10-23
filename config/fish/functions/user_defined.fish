00346c94172f5eaee3a63d5a604705755f7a6765
Dimitar Haralanov <dharalanov@me.com>
2016-10-23 15:53:37 +0300

    Add description to user defined functions

--- config/fish/functions/user_defined.fish
+++ config/fish/functions/user_defined.fish
@@ -1,8 +1,8 @@
-function ct
+function ct --description 'Generate ctags'
   ctags -R -f ./.git/tags .
 end
 
-function gl
+function gl --description 'Git fuzzy find and checkout local branch'
 	if command git rev-parse --is-inside-work-tree >/dev/null 2>&1
 		git branch -vv | fzf | awk '{print $1}' | sed "s/.* //" | read -l branch;
     and git checkout $branch
@@ -11,7 +11,7 @@ function gl
 	end
 end
 
-function gr
+function gr --description 'Git fuzzy find and checkout remote branch'
 	if command git rev-parse --is-inside-work-tree >/dev/null 2>&1
 		git branch --all | grep -v HEAD | fzf | sed "s/.* //" | sed "s#remotes/[^/]*/##" | read -l branch;
     and git checkout $branch
@@ -20,7 +20,7 @@ function gr
 	end
 end
 
-function fkill
+function fkill --description 'Fuzzy kill by PID'
 	ps -ef | sed 1d | fzf | awk '{print $2}' | read -l pid
   if [ "x$pid" != "x" ]
     kill -9 $pid
