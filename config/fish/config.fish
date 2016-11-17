beba3c3f5a5e08fe300d743a7767ab70015b8cd0
Dimitar Haralanov <dharalanov@me.com>
2016-11-17 22:46:59 +0200

    Update fish background match color

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -22,7 +22,7 @@ set -x LANG en_US.UTF-8
 set -x LC_CTYPE en_US.UTF-8
 
 # Search hightlight color
-set fish_color_search_match --background=d98c8c
+set fish_color_search_match --background=771503
 
 # Shortcuts
 alias g='git'
