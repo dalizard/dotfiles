26ff9863c150b72a502e120826030186835efcf1
Dimitar Haralanov <dharalanov@me.com>
2017-01-23 09:15:22 +0200

    Use fzf-tmux over fzf

--- config/fish/functions/user_defined.fish
+++ config/fish/functions/user_defined.fish
@@ -21,7 +21,7 @@ function gr --description 'Git fuzzy find and checkout remote branch'
 end
 
 function fkill --description 'Fuzzy kill by PID'
-	ps -ef | sed 1d | fzf | awk '{print $2}' | read -l pid
+	ps -ef | sed 1d | fzf-tmux | awk '{print $2}' | read -l pid
   if [ "x$pid" != "x" ]
     kill -9 $pid
   end
