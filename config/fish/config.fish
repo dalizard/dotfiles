99a539dcda675b0285563ed2ee4d63c361ad3fa0
Dimitar Haralanov <dharalanov@me.com>
2017-02-16 10:00:39 +0100

    Add git current branch to clipboard alias

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -30,6 +30,7 @@ alias ll='ls -alGF'
 alias gg='git status'
 alias be='bundle exec'
 alias gh="git log --pretty=format:'%h' -n 1 | pbcopy"
+alias gb="git rev-parse --abbrev-ref HEAD | pbcopy"
 
 # hub is aliased as git
 eval (hub alias -s)
