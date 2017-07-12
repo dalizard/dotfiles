bec46cf041330410b0c175d5ded93f5ffbc0b618
Dimitar Haralanov <dharalanov@me.com>
2017-07-12 12:17:58 +0300

    Add gir() and fssh() fish functions

--- config/fish/functions/user_defined.fish
+++ config/fish/functions/user_defined.fish
@@ -26,3 +26,19 @@ function fkill --description 'Fuzzy kill by PID'
     kill -9 $pid
   end
 end
+
+
+function gir --description 'Git fuzzy-find commit SHA and interactively rebase'
+	if command git rev-parse --is-inside-work-tree >/dev/null 2>&1
+    git log --color=always --pretty=format:"%C(yellow)%h %Cblue%>(12)%ar %Cgreen%<(7)%aN%Cred%d %Creset%s" --abbrev-commit --reverse | fzf --tac +s -e --ansi | awk '{print $1;}' | read -l commit
+    if test -n "$commit"
+      git rebase -i $commit^
+    end
+  else
+    echo Not a git repo.
+  end
+end
+
+function fssh --description "Fuzzy-find ssh host and ssh into it"
+  ag '^host [^*]' ~/.ssh/config | cut -d ' ' -f 2 | fzf | xargs -o ssh
+end
