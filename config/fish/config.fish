6e8872dead00f23cba9f6c11661c3b65418926b3
Dimitar Haralanov <dharalanov@me.com>
2018-02-16 10:24:19 +0200

    Drop newline from `gb`

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -33,7 +33,7 @@ alias ll='ls -alGF'
 alias gg='git status'
 alias be='bundle exec'
 alias gh="git log --pretty=format:'%h' -n 1 | pbcopy"
-alias gb="git rev-parse --abbrev-ref HEAD | pbcopy"
+alias gb="git rev-parse --abbrev-ref HEAD | tr -d '\n' | pbcopy"
 
 # hub is aliased as git
 eval (hub alias -s)
