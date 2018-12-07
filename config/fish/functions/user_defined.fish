45fa315ac932a9c33cec449ae099b0ef4a35c379
Dimitar Haralanov <dharalanov@me.com>
2018-12-07 11:29:54 +0200

    Add tunup for setting up an HTTP tunnel

--- config/fish/functions/user_defined.fish
+++ config/fish/functions/user_defined.fish
@@ -42,3 +42,9 @@ end
 function fssh --description "Fuzzy-find ssh host and ssh into it"
   rg '^Host [^*]' ~/.ssh/config | cut -d ' ' -f 2 | fzf --height 10% | xargs -o ssh
 end
+
+function tunup --description "Initialize SSH tunnel for exposing local web servers"
+  set port $argv
+	echo "Forwarding rb.forebits.com:5000 to local port $port"
+	ssh -R 5000:127.0.0.1:$port gollum
+end
