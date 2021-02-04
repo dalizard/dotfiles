a2e363281ac9a48c8090edebccd4996b2edb7655
Dimitar Haralanov <dharalanov@me.com>
2021-02-04 13:19:15 +0200

    Make some small experimental changes to `gr`

--- config/fish/functions/user_defined.fish
+++ config/fish/functions/user_defined.fish
@@ -12,12 +12,19 @@ function gl --description 'Git fuzzy find and checkout local branch'
 end
 
 function gr --description 'Git fuzzy-find and checkout remote branch'
-	if command git rev-parse --is-inside-work-tree >/dev/null 2>&1
-		git branch -r | grep -v HEAD | fzf-tmux | sed "s/.* //" | sed "s#origin/##" | read -l branch;
-    and git checkout $branch
-	else
-		echo Not a git repo.
-	end
+  test -n "$FZF_TMUX_HEIGHT"; or set FZF_TMUX_HEIGHT 10%
+  begin
+    set -lx FZF_DEFAULT_OPTS "--height $FZF_TMUX_HEIGHT $FZF_DEFAULT_OPTS $FZF_CTRL_R_OPTS"
+
+    if command git rev-parse --is-inside-work-tree >/dev/null 2>&1
+      git branch -r | grep -v HEAD | eval (__fzfcmd) | sed "s/.* //" | sed "s#origin/##" | read -l branch
+      and git checkout $branch
+      commandline -f repaint
+    else
+      echo Not a git repo.
+    end
+  end
+  commandline -f repaint
 end
 
 function fkill --description 'Fuzzy kill by PID'
