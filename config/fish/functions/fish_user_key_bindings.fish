7010dea7b1b9a3915687cf7f284f205230f35090
Dimitar Haralanov <dharalanov@me.com>
2021-02-04 10:34:16 +0200

    Drop unused shortcut

--- config/fish/functions/fish_user_key_bindings.fish
+++ config/fish/functions/fish_user_key_bindings.fish
@@ -1,8 +1,3 @@
 function fish_user_key_bindings
-  # The reverse search in fish works in reverse. You first type what you want to
-  # search for, then you press the Up arrow. Rebind it to ^E, so I can remember
-  # it better.
-  bind \ce history-search-backward
+  fzf_key_bindings
 end
-
-fzf_key_bindings
