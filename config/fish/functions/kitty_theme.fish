7deda401637cc767280b259d16a4743574167627
Dimitar Haralanov <dharalanov@me.com>
2022-02-04 19:17:00 +0200

    Oops. Fix kitty_theme()

--- config/fish/functions/kitty_theme.fish
+++ config/fish/functions/kitty_theme.fish
@@ -1,19 +1,25 @@
 function kitty_theme -d "Change kitty theme" -a theme_name
-  ## Do nothing if there is no theme specified
-  #if [ -z "$theme_name" ]
-  #  return
-  #end
+  # Do nothing if there is no theme specified
+  if [ -z "$theme_name" ]
+    return
+  end
 
-  ## Path to search for theme conf files
-  #set -l theme_folder (realpath ~/.dotfiles/config/kitty/themes)
+  set -l _theme_name (string replace -r '^.' (string sub -l 1 $theme_name | string upper) $theme_name)
 
-  ## Full path to theme
-  #set -l theme_path "$theme_folder/$theme_name.conf"
+  if test $OS_NAME = 'darwin'
+    kitty +kitten themes --reload-in=all $_theme_name
+  else
+    # Path to search for theme conf files
+    set -l theme_folder (realpath ~/.dotfiles/config/kitty/themes)
 
-  ## If theme exists, change theme
-  #if [ -e "$theme_path" ]
-  #  # Change for current session
-  #  kitty @ set-colors --all --configured $theme_path
-  #end
-  kitty +kitten themes --reload-in=all (string replace -r '^.' (string sub -l 1 $theme_name | string upper) $theme_name)
+    # Full path to theme
+    set -l theme_path "$theme_folder/$theme_name.conf"
+
+    # If theme exists, change theme
+    if [ -e "$theme_path" ]
+      # Change for current session
+      kitty +kitten themes --dump-theme $_theme_name > ~/.config/kitty/current-theme.conf
+      kitty @ set-colors --all --configured $theme_path
+    end
+  end
 end
