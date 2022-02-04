15ac952795121476ea6c146df114e3a818e20ffc
Dimitar Haralanov <dharalanov@me.com>
2022-02-04 10:39:05 +0200

    (WIP) Fix kitty theme colors

--- config/fish/functions/kitty_theme.fish
+++ config/fish/functions/kitty_theme.fish
@@ -1,18 +1,19 @@
 function kitty_theme -d "Change kitty theme" -a theme_name
-  # Do nothing if there is no theme specified
-  if [ -z "$theme_name" ]
-    return
-  end
+  ## Do nothing if there is no theme specified
+  #if [ -z "$theme_name" ]
+  #  return
+  #end
 
-  # Path to search for theme conf files
-  set -l theme_folder (realpath ~/.dotfiles/config/kitty/themes)
+  ## Path to search for theme conf files
+  #set -l theme_folder (realpath ~/.dotfiles/config/kitty/themes)
 
-  # Full path to theme
-  set -l theme_path "$theme_folder/$theme_name.conf"
+  ## Full path to theme
+  #set -l theme_path "$theme_folder/$theme_name.conf"
 
-  # If theme exists, change theme
-  if [ -e "$theme_path" ]
-    # Change for current session
-    kitty @ set-colors --all --configured $theme_path
-  end
+  ## If theme exists, change theme
+  #if [ -e "$theme_path" ]
+  #  # Change for current session
+  #  kitty @ set-colors --all --configured $theme_path
+  #end
+  kitty +kitten themes --reload-in=all (string replace -r '^.' (string sub -l 1 $theme_name | string upper) $theme_name)
 end
