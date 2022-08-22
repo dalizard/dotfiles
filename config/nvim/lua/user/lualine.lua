8f952a96cb7afff25df762af9d754ff7d471b10e
Dimitar Haralanov <dharalanov@me.com>
2022-08-22 10:45:58 +0300

    Drop git diff from lualine

--- config/nvim/lua/user/lualine.lua
+++ config/nvim/lua/user/lualine.lua
@@ -24,7 +24,7 @@ lualine.setup {
   },
   sections = {
     lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_b = {'branch', 'diagnostics'},
     lualine_c = {'filename'},
     lualine_x = {'encoding', 'filetype'},
     lualine_y = {'progress'},
