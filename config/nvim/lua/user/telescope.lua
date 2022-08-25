02db27c10b115c018d364094bde00571a616c151
Dimitar Haralanov <dharalanov@me.com>
2022-08-25 14:10:32 +0300

    Drop unneeded require call

--- config/nvim/lua/user/telescope.lua
+++ config/nvim/lua/user/telescope.lua
@@ -6,7 +6,7 @@ end
 local actions = require "telescope.actions"
 local action_layout = require "telescope.actions.layout"
 
-require('telescope').setup{
+telescope.setup{
   defaults = {
     mappings = {
       i = {
