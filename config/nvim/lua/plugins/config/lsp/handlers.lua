a5bf110ab5e108cd9b86958d4a668be6e6050a5b
Dimitar Haralanov <dharalanov@me.com>
2022-10-24 22:48:37 +0200

    Some small updates

--- config/nvim/lua/plugins/config/lsp/handlers.lua
+++ config/nvim/lua/plugins/config/lsp/handlers.lua
@@ -42,7 +42,7 @@ local function lsp_keymaps(bufnr)
 
   -- Generate LSP functionality
   vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-  vim.keymap.set("n", "ge", vim.lsp.buf.rename, opts)
+  vim.keymap.set("n", "gr", vim.lsp.buf.rename, opts)
   vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
   vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
   vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
