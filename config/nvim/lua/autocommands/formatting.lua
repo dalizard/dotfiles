73c6819310cef7384c81df75d0bf6f335bf3b8ec
Dimitar Haralanov <dharalanov@me.com>
2022-10-27 10:47:40 +0200

    Disable automatic comments insertion

--- config/nvim/lua/autocommands/formatting.lua
+++ config/nvim/lua/autocommands/formatting.lua
@@ -1,7 +1,7 @@
 vim.api.nvim_create_augroup("_formatting", { clear = true })
 
 -- Strip trailing whitespaces on save
-vim.api.nvim_create_autocmd("BufWritePre", {
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   group = "_formatting",
   pattern = "*",
   command = "%s/\\s\\+$//e"
@@ -20,5 +20,15 @@ vim.api.nvim_create_autocmd({ "WinEnter", "InsertLeave" }, {
   pattern = "*",
   callback = function()
     vim.cmd("set cursorline")
+  end,
+})
+
+-- Diable automatic comment insertion
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  group = "_formatting",
+  pattern = "*",
+  callback = function()
+    -- For some reason formatoptions:remove is not working, so...
+    vim.cmd("set formatoptions-=cro")
   end,
 })
