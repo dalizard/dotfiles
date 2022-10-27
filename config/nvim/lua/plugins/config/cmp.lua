1416265ce7c9264753a54a75a938da4e9ee44960
Dimitar Haralanov <dharalanov@me.com>
2022-10-27 10:15:58 +0200

    Trigger LSP suggestions in 3 chars instead of 4

--- config/nvim/lua/plugins/config/cmp.lua
+++ config/nvim/lua/plugins/config/cmp.lua
@@ -115,10 +115,10 @@ cmp.setup({
     end,
   },
   sources = cmp.config.sources({
-    { name = 'nvim_lsp', keyword_length = 4 },
-    { name = 'nvim_lua', keyword_length = 4 },
+    { name = 'nvim_lsp', keyword_length = 3 },
+    { name = 'nvim_lua', keyword_length = 3 },
     { name = 'luasnip', keyword_length = 3 },
-    { name = 'buffer', keyword_length = 4 },
+    { name = 'buffer', keyword_length = 3 },
     { name = 'path' },
   }),
   experimental = {
