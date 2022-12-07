90cb1723d8966cc0f9c562089255a7ec520d6176
Dimitar Haralanov <dharalanov@me.com>
2022-12-07 23:57:24 +0100

    Set a few things

--- config/nvim/lua/plugins/config/lualine.lua
+++ config/nvim/lua/plugins/config/lualine.lua
@@ -6,7 +6,7 @@ end
 lualine.setup {
   options = {
     icons_enabled = true,
-    theme = 'gruvbox',
+    theme = 'lagadath',
     component_separators = { left = '', right = ''},
     section_separators = { left = '', right = ''},
     disabled_filetypes = {
