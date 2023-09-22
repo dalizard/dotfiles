f23c015c73847f8a514e3914e0cf183cae17f8b1
Dimitar Haralanov <dharalanov@me.com>
2023-09-22 11:51:33 +0200

    Migrate from packer.nvim to lazy.nvim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,13 +1,8 @@
 -- Disable the netrw plugin
 vim.g.loaded_netrwPlugin = 1
 
-local impatient_loaded, impatient = pcall(require, "impatient")
-if impatient_loaded then
-  impatient.enable_profile()
-end
-
-require "autocommands"
 require "keymaps"
-require "options"
 require "plugins"
+require "autocommands"
+require "options"
 require "globals"
--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -1,159 +1,111 @@
--- Bootstrap packer
-local fn = vim.fn
-local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim',
-    install_path })
-  print "Installing packer - close and reopen Neovim"
-  vim.cmd [[packadd packer.nvim]]
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
--- Use a protected call so we don't error out on first use
-local status_ok, packer = pcall(require, "packer")
-if not status_ok then
-  return
-end
-
--- Automatically source and re-compile packer whenever you save this init.lua
-local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
-vim.api.nvim_create_autocmd('BufWritePost', {
-  command = 'source <afile> | PackerCompile',
-  group = packer_group,
-  pattern = vim.fn.expand '$MYVIMRC',
-})
-
--- Have packer use a popup window
-packer.init {
-  -- snapshot = "August 17, 2022",
-  snapshot_path = fn.stdpath "config" .. "/snapshots",
-  max_jobs = 50,
-  display = {
-    open_fn = function()
-      return require("packer.util").float { border = "rounded" }
-    end,
-    prompt_border = "rounded", -- Border style of prompt popups.
-  },
-}
-
-return packer.startup(function(use)
-  -- Plugin Manager
-  use "wbthomason/packer.nvim" -- have packer manage itself
-
+require("lazy").setup({
   -- LSP
-  use {
+  {
     "williamboman/mason.nvim",
     config = function() require("plugins.config.mason") end
-  }
-  use {
+  },
+  {
     "williamboman/mason-lspconfig.nvim",
-    after = "mason.nvim"
-  }
-  use {
+    dependencies = "mason.nvim"
+  },
+  {
     "neovim/nvim-lspconfig",
     config = function() require("plugins.config.lsp") end,
-    after = "mason-lspconfig.nvim"
-  }
-
+    dependencies = "mason-lspconfig.nvim"
+  },
   -- Completion
-  use {
+  {
     "hrsh7th/nvim-cmp", -- core plugin
     config = function() require("plugins.config.cmp") end
-  }
-  use "hrsh7th/cmp-buffer" -- completes words from the current buffer
-  use "hrsh7th/cmp-nvim-lsp" -- LSP support
-  use "hrsh7th/cmp-nvim-lua" -- neovim Lua completion
-  use "hrsh7th/cmp-path" -- complete files
-  use "saadparwaiz1/cmp_luasnip"
-
+  },
+  "hrsh7th/cmp-buffer",   -- completes words from the current buffer
+  "hrsh7th/cmp-nvim-lsp", -- LSP support
+  "hrsh7th/cmp-nvim-lua", -- neovim Lua completion
+  "hrsh7th/cmp-path",     -- complete files
+  "saadparwaiz1/cmp_luasnip",
   -- Syntax/Treesitter
-  use {
+  {
     "nvim-treesitter/nvim-treesitter",
-    run = function() require("nvim-treesitter.install").update({ with_sync = true }) end,
+    build = function() require("nvim-treesitter.install").update({ with_sync = true }) end,
     config = function() require("plugins.config.treesitter") end
-  }
-
-  use "nvim-treesitter/playground"
-
+  },
+  "nvim-treesitter/playground",
   -- Fuzzy Finder/Telescope
-  use {
+  {
     "nvim-telescope/telescope.nvim",
-    run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
-    requires = { { 'nvim-lua/plenary.nvim' } },
+    build = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
+    dependencies = "nvim-lua/plenary.nvim",
     config = function() require("plugins.config.telescope") end
-  }
-  use "nvim-telescope/telescope-file-browser.nvim"
-  use {
+  },
+  "nvim-telescope/telescope-file-browser.nvim",
+  {
     "nvim-telescope/telescope-fzf-native.nvim",
-    run = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build"
-  }
-
+    build =
+    "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build"
+  },
   -- Snippet
-  use "L3MON4D3/LuaSnip"
-
+  "L3MON4D3/LuaSnip",
   -- Colorscheme
-  use "dalizard/lagadath"
-
-  use {
+  "dalizard/lagadath",
+  {
     "kyazdani42/nvim-web-devicons",
     config = function() require("plugins.config.nvim-web-devicons") end
-  }
-
+  },
   -- Statusline
-  use {
+  {
     "nvim-lualine/lualine.nvim",
     config = function() require("plugins.config.lualine") end
-  }
-
+  },
   -- Git
-  use {
+  {
     "lewis6991/gitsigns.nvim",
     config = function() require("plugins.config.gitsigns") end
-  }
-  use "tpope/vim-fugitive"
-  use "tpope/vim-rhubarb"
-
+  },
+  "tpope/vim-fugitive",
+  "tpope/vim-rhubarb",
   -- Tests
-  use {
+  {
     "vim-test/vim-test",
     config = function() require("plugins.config.vim-test") end
-  }
-
+  },
   -- Ruby on Rails
-  use "tpope/vim-rails"
-
+  "tpope/vim-rails",
   -- File Explorer
-  use {
+  {
     "kyazdani42/nvim-tree.lua",
     config = function() require("plugins.config.nvim-tree") end
-  }
-
+  },
   -- Editing Support
-  use {
+  {
     "windwp/nvim-autopairs",
     config = function() require("plugins.config.autopairs") end
-  }
-
+  },
   -- Utility
-  use "lewis6991/impatient.nvim"
-  use {
+  {
     "kylechui/nvim-surround",
     config = function() require("plugins.config.nvim-surround") end
-  }
-
-  use {
+  },
+  {
     "NvChad/nvim-colorizer.lua",
     config = function() require("plugins.config.nvim-colorizer") end
-  }
-
-  use {
+  },
+  {
     "euclio/vim-markdown-composer",
-    run = 'cargo build --release',
+    build = 'cargo build --release',
     config = function() require("plugins.config.vim-markdown-composer") end
   }
-
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Keep this at the end
-  if PACKER_BOOTSTRAP then
-    require('packer').sync()
-  end
-end)
+})
