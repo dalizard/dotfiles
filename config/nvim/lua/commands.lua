4925b913ee854fab24565b112893128d73407fbb
Dimitar Haralanov <dharalanov@me.com>
2023-10-24 11:56:54 +0200

    Oops. Fix inline Lua function

--- config/nvim/lua/commands.lua
+++ config/nvim/lua/commands.lua
@@ -1,1 +1,1 @@
-vim.api.nvim_create_user_command('AC', function(); vim.cmd('vsplit ' .. vim.fn.eval('rails#buffer().alternate()')); end, {})
+vim.api.nvim_create_user_command('AC', function() vim.cmd('vsplit ' .. vim.fn.eval('rails#buffer().alternate()')) end, {})
