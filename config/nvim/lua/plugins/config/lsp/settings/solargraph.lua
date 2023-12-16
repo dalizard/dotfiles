628aaa2d146f8d4f1a25f9e601ec874ef158e6f3
Dimitar Haralanov <dharalanov@me.com>
2023-12-16 22:08:31 +0100

    Get back to using solargraph

---
+++ config/nvim/lua/plugins/config/lsp/settings/solargraph.lua
@@ -0,0 +1,13 @@
+return {
+  settings = {
+    solargraph = {
+      autoformat = true,
+      completion = true,
+      diagnostic = true,
+      folding = true,
+      references = true,
+      rename = true,
+      symbols = true
+    }
+  }
+}
