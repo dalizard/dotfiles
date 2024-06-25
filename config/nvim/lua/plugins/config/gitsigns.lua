c7a71de3279a147d903fcd90f07bf594b5f72aa7
Dimitar Haralanov <dharalanov@me.com>
2024-06-25 09:24:51 +0200

    Update gitsigns config

--- config/nvim/lua/plugins/config/gitsigns.lua
+++ config/nvim/lua/plugins/config/gitsigns.lua
@@ -5,11 +5,11 @@ end
 
 gitsigns.setup {
   signs = {
-    add          = {hl = 'GitSignsAdd'   , text = '▎', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
-    change       = {hl = 'GitSignsChange', text = '▎', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
-    delete       = {hl = 'GitSignsDelete', text = '﹅', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-    topdelete    = {hl = 'GitSignsDelete', text = '﹅', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-    changedelete = {hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+    add          = {text = '▎'},
+    change       = {text = '▎'},
+    delete       = {text = '﹅'},
+    topdelete    = {text = '﹅'},
+    changedelete = {text = '~'},
   },
   signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
   numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
@@ -39,9 +39,6 @@ gitsigns.setup {
     relative = 'cursor',
     row = 0,
     col = 1
-  },
-  yadm = {
-    enable = false
   },
   on_attach = function(bufnr)
     local gs = package.loaded.gitsigns
