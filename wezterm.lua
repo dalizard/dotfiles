11a82c95c1555a59eea16741e0c29b6ad881f5df
Dimitar Haralanov <dharalanov@me.com>
2024-10-23 10:56:44 +0200

    Add wezterm config

---
+++ wezterm.lua
@@ -0,0 +1,16 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = 'Gruvbox Dark (Gogh)'
+
+config.enable_tab_bar = false
+config.audible_bell = "Disabled"
+
+-- and finally, return the configuration to wezterm
+return config
