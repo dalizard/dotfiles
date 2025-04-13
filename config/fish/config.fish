e5ff0c5ba9ae6b0af4a428bf95d5ba26803e8f2d
Dimitar Haralanov <dharalanov@me.com>
2025-04-13 07:39:36 +0200

    Add OrbStack integration to fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -108,3 +108,7 @@ end
 
 # Activate mise
 mise activate fish | source
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
