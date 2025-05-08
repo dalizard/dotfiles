f4f1072cc0d2c65fab71fe11e7b18fe1370d0850
Dimitar Haralanov <dharalanov@me.com>
2025-05-08 12:27:46 +0200

    Update LSP config to satisfy latest mason-lspconfig

---
+++ config/nvim/after/lsp/solargraph.lua
@@ -0,0 +1,21 @@
+return {
+  cmd = {
+    "mise",
+    "x",
+    "--",
+    "solargraph",
+    "stdio",
+  },
+  settings = {
+    solargraph = {
+      autoformat = true,
+      completion = true,
+      diagnostics = true,
+      definitions = true,
+      folding = true,
+      references = true,
+      rename = true,
+      symbols = true
+    }
+  }
+}
--- config/nvim/lua/plugins/config/lsp/config.lua
+++ config/nvim/lua/plugins/config/lsp/config.lua
@@ -1,6 +1,7 @@
-local lspconfig = require("lspconfig")
+local mason = require("mason")
 local mason_lspconfig = require("mason-lspconfig")
 
+mason.setup()
 mason_lspconfig.setup({
   ensure_installed = {
     "bashls",
@@ -21,40 +22,7 @@ mason_lspconfig.setup({
   }
 })
 
-local defaults = {
+vim.lsp.config("*", {
   on_attach = require("plugins.config.lsp.handlers").on_attach,
   capabilities = require("plugins.config.lsp.handlers").capabilities,
-}
-
-mason_lspconfig.setup_handlers({
-  function(server_name) -- default handler
-    require("lspconfig")[server_name].setup(defaults)
-  end,
-
-  ["jsonls"] = function()
-    local options = vim.tbl_deep_extend("force", require("plugins.config.lsp.settings.jsonls"), defaults)
-    lspconfig.jsonls.setup(options)
-  end,
-
-  ["lua_ls"] = function()
-    local options = vim.tbl_deep_extend("force", require("plugins.config.lsp.settings.lua_ls"), defaults)
-    lspconfig.lua_ls.setup(options)
-  end,
-
-  ["solargraph"] = function()
-    local options = vim.tbl_deep_extend("force", require("plugins.config.lsp.settings.solargraph"), defaults)
-    lspconfig.solargraph.setup(options)
-  end,
-
-  ["golangci_lint_ls"] = function()
-    local configs = require 'lspconfig/configs'
-    local options = require("plugins.config.lsp.settings.golangci_lint_ls")
-    local config, lsp_config  = unpack(options)
-
-    if not configs.golangcilsp then
-      configs.golangcilsp = lsp_config
-    end
-
-    lspconfig.golangci_lint_ls.setup(config)
-  end
 })
