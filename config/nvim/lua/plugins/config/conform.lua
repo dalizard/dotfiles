d4053a30d5c73a77480a86ef6562f78334259374
Dimitar Haralanov <dharalanov@me.com>
2025-09-04 17:55:34 +0200

    Increase the auto-format for conform.lua to 3000ms

--- config/nvim/lua/plugins/config/conform.lua
+++ config/nvim/lua/plugins/config/conform.lua
@@ -9,7 +9,7 @@ conform.setup({
   },
   format_on_save = {
     -- These options will be passed to conform.format()
-    timeout_ms = 500,
+    timeout_ms = 3000,
     lsp_format = "fallback",
   },
 })
