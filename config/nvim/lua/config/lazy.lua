dee9e56c8971895b38dca5f0de82f0cdeee73bff
Dimitar Haralanov <dharalanov@me.com>
2025-09-23 10:31:24 +0200

    Disable automatic check for new plugin versions

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -33,6 +33,6 @@ require("lazy").setup({
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "habamax" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
+  -- do not check for plugin updates
+  checker = { enabled = false },
 })
