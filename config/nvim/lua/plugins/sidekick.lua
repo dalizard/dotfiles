bb66a8570adde418e76728ae3d86015fc6cb76d4
Dimitar Haralanov <dharalanov@me.com>
2025-10-17 12:23:35 +0200

    Update sidekick config

--- config/nvim/lua/plugins/sidekick.lua
+++ config/nvim/lua/plugins/sidekick.lua
@@ -1,19 +1,52 @@
 return {
   "folke/sidekick.nvim",
   opts = {
-    -- add any options here
     cli = {
       mux = {
         backend = "tmux",
-        enabled = true,
+        enabled = false,
       },
     },
   },
   keys = {
     {
-      "<leader>dc",
-      function() require("sidekick.cli").toggle({ name = "claude", focus = true }) end,
-      desc = "Sidekick Toggle Claude",
+      "<c-.>",
+      function() require("sidekick.cli").toggle() end,
+      desc = "Sidekick Toggle",
+      mode = { "n", "t", "i", "x" },
+    },
+    {
+      "<leader>aa",
+      function() require("sidekick.cli").toggle() end,
+      desc = "Sidekick Toggle CLI",
+    },
+    {
+      "<leader>as",
+      function() require("sidekick.cli").select({ filter = { installed = true } }) end,
+      desc = "Select CLI",
+    },
+    {
+      "<leader>ad",
+      function() require("sidekick.cli").close() end,
+      desc = "Detach a CLI Session",
+    },
+    {
+      "<leader>at",
+      function() require("sidekick.cli").send({ msg = "{this}" }) end,
+      mode = { "x", "n" },
+      desc = "Send This",
+    },
+    {
+      "<leader>av",
+      function() require("sidekick.cli").send({ msg = "{selection}" }) end,
+      mode = { "x" },
+      desc = "Send Visual Selection",
+    },
+    {
+      "<leader>ap",
+      function() require("sidekick.cli").prompt() end,
+      mode = { "n", "x" },
+      desc = "Sidekick Select Prompt",
     },
   }
 }
