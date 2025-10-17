2b120ddb06ecc699a851b07b1ca84a473ac1b508
Dimitar Haralanov <dharalanov@me.com>
2025-10-17 11:35:00 +0200

    Add blink-cmp config

--- config/nvim/lua/plugins/lspconfig.lua
+++ config/nvim/lua/plugins/lspconfig.lua
@@ -1,6 +1,57 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    dependencies = "mason-lspconfig.nvim"
-  }
+    dependencies = "mason-lspconfig.nvim",
+    config = function()
+      local function on_lsp_attach(client, bufnr)
+        local function map(mode, lhs, rhs, opts)
+          vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = "LSP " .. opts })
+        end
+
+        map("n", "K", function() vim.lsp.buf.hover({ border = "rounded" }) end, "Display hover info")
+        map("n", "gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+        map("n", "gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+        map("n", "gi", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
+        map("n", "gt", vim.lsp.buf.type_definition, "[G]oto [T]ype definition")
+        map("n", "ga", vim.lsp.buf.code_action, "[G]et [A]ctions")
+        map("x", "ga", vim.lsp.buf.code_action, "[G]et [A]ctions")
+
+        map("n", "gl", vim.diagnostic.open_float, "Show diagnostics")
+
+        map("n", "<space>gn", vim.lsp.buf.rename, "[R]ename")
+        map("n", "<space>go", vim.lsp.buf.document_symbol, "[G]et Symbols (document)")
+        map("n", "<space>gr", vim.lsp.buf.references, "[G]et [R]eferences")
+        map("n", "<space>gS", vim.lsp.buf.workspace_symbol, "[G]et [S]ymbols (workspace)")
+
+
+        if client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint, bufnr) then
+          map("n", "<leader>th", function()
+            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = bufnr }), { bufnr = bufnr })
+          end, "[T]oggle [H]int")
+          client.server_capabilities.semanticTokensProvider = nil
+        end
+      end
+
+      local client_capabilities = vim.lsp.protocol.make_client_capabilities()
+
+      vim.diagnostic.config({
+        virtual_text = {
+          prefix = "",
+        },
+        signs = {
+          text = {
+            [vim.diagnostic.severity.ERROR] = '󰅚',
+            [vim.diagnostic.severity.WARN] = '󰀪',
+            [vim.diagnostic.severity.HINT] = '󰙎',
+            [vim.diagnostic.severity.INFO] = '󰋽',
+          },
+        },
+      })
+
+      vim.lsp.config('*', {
+        capabilities = require("blink.cmp").get_lsp_capabilities(client_capabilities),
+        on_attach = on_lsp_attach,
+      })
+    end
+  },
 }
