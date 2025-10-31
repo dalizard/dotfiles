888a0c12c932486a47c14982129d80995862c1d7
Dimitar Haralanov <dharalanov@me.com>
2025-10-31 11:40:48 +0100

    Add nvim-ts-autotag

---
+++ config/nvim/lua/plugins/nvim-ts-autotag.lua
@@ -0,0 +1,10 @@
+return {
+  "windwp/nvim-ts-autotag",
+  opts = {
+    opts = {
+      enable_close = true,        -- Auto close tags
+      enable_rename = true,       -- Auto rename pairs of tags
+      enable_close_on_slash = false -- Auto close on trailing </
+    }
+  }
+}
