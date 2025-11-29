9996bf6bac47aec3f4787c9e8caf13e63ef99c57
Dimitar Haralanov <dharalanov@me.com>
2025-11-29 14:17:55 +0100

    Tweak a few things

--- config/nvim/after/lsp/ruby_lsp.lua
+++ config/nvim/after/lsp/ruby_lsp.lua
@@ -1,13 +1,13 @@
 return {
   filetypes = { "ruby" },
 
-  cmd = { "mise x -- ruby-lsp" },
+  cmd = { "mise", "x", "--", "ruby-lsp" },
 
   root_markers = { "Gemfile", ".git" },
 
   init_options = {
-    formatter = 'standard',
-    linters = { 'standard' },
+    formatter = 'rubocop',
+    linters = { 'rubocop' },
     addonSettings = {
       ["Ruby LSP Rails"] = {
         enablePendingMigrationsPrompt = false,
--- config/nvim/lua/plugins/lspconfig.lua
+++ config/nvim/lua/plugins/lspconfig.lua
@@ -9,10 +9,10 @@ return {
         end
 
         map("n", "K", function() vim.lsp.buf.hover({ border = "rounded" }) end, "Display hover info")
-        map("n", "gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
-        map("n", "gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-        map("n", "gi", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
-        map("n", "gt", vim.lsp.buf.type_definition, "[G]oto [T]ype definition")
+        map("n", "gd", Snacks.picker.lsp_definitions, "[G]oto [D]efinition")
+        map("n", "gD", Snacks.picker.lsp_declarations, "[G]oto [D]eclaration")
+        map("n", "gi", Snacks.picker.lsp_implementations, "[G]oto [I]mplementation")
+        map("n", "gt", Snacks.picker.lsp_type_definitions, "[G]oto [T]ype definition")
         map("n", "ga", vim.lsp.buf.code_action, "[G]et [A]ctions")
         map("x", "ga", vim.lsp.buf.code_action, "[G]et [A]ctions")
 
@@ -21,8 +21,8 @@ return {
         map("n", "<space>gr", vim.lsp.buf.rename, "[R]ename")
         map("n", "<space>gd", Snacks.picker.diagnostics_buffer, "[G]get buffer [D]iagnostics")
         map("n", "<space>gs", Snacks.picker.lsp_symbols, "[G]et [S]ymbols (document)")
-        map("n", "<space>gr", Snacks.picker.lsp_references, "[G]et [R]eferences")
         map("n", "<space>gS", Snacks.picker.lsp_workspace_symbols, "[G]et [S]ymbols (workspace)")
+        map("n", "<space>gr", Snacks.picker.lsp_references, "[G]et [R]eferences")
 
         if client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint, bufnr) then
           map("n", "<leader>th", function()
@@ -51,7 +51,7 @@ return {
         on_attach = on_lsp_attach,
       })
 
-      -- vim.lsp.set_log_level("off")
+      vim.lsp.set_log_level("off")
     end
   },
 }
