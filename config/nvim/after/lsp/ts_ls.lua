c7385ec874634a4148e76e460fdb218b4dc4fb6a
Dimitar Haralanov <dharalanov@me.com>
2025-12-05 12:53:05 +0100

    Add TS support in Vue.js

--- config/nvim/after/lsp/ts_ls.lua
+++ config/nvim/after/lsp/ts_ls.lua
@@ -1,3 +1,19 @@
+local vue_language_server_path = vim.fn.stdpath('data') ..
+    "/mason/packages/vue-language-server/node_modules/@vue/language-server"
+
+local vue_plugin = {
+  name = '@vue/typescript-plugin',
+  location = vue_language_server_path,
+  languages = { 'vue' },
+  configNamespace = 'typescript',
+}
+
 return {
-  workspace_required = false
+  workspace_required = false,
+  filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+  init_options = {
+    plugins = {
+      vue_plugin,
+    }
+  }
 }
