3655ad4ee16b2f6a25bfd7d732ab856d062fbc19
Dimitar Haralanov <dharalanov@me.com>
2026-01-23 15:09:05 +0100

    Use LspAttach instead of on_attach

--- config/nvim/lua/plugins/lspconfig.lua
+++ config/nvim/lua/plugins/lspconfig.lua
@@ -1,39 +1,7 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    dependencies = "mason-lspconfig.nvim",
     config = function()
-      local Snacks = require("snacks")
-
-      local function on_lsp_attach(client, bufnr)
-        local function map(mode, lhs, rhs, opts)
-          vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = "LSP " .. opts })
-        end
-
-        map("n", "K", function() vim.lsp.buf.hover({ border = "rounded" }) end, "Display hover info")
-        map("n", "gd", Snacks.picker.lsp_definitions, "[G]oto [D]efinition")
-        map("n", "gD", Snacks.picker.lsp_declarations, "[G]oto [D]eclaration")
-        map("n", "gi", Snacks.picker.lsp_implementations, "[G]oto [I]mplementation")
-        map("n", "gt", Snacks.picker.lsp_type_definitions, "[G]oto [T]ype definition")
-        map("n", "ga", vim.lsp.buf.code_action, "[G]et [A]ctions")
-        map("x", "ga", vim.lsp.buf.code_action, "[G]et [A]ctions")
-
-        map("n", "gl", vim.diagnostic.open_float, "Show diagnostics")
-
-        map("n", "<space>gr", vim.lsp.buf.rename, "[R]ename")
-        map("n", "<space>gd", Snacks.picker.diagnostics_buffer, "[G]get buffer [D]iagnostics")
-        map("n", "<space>gs", Snacks.picker.lsp_symbols, "[G]et [S]ymbols (document)")
-        map("n", "<space>gS", Snacks.picker.lsp_workspace_symbols, "[G]et [S]ymbols (workspace)")
-        map("n", "<space>gr", Snacks.picker.lsp_references, "[G]et [R]eferences")
-
-        if client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint, bufnr) then
-          map("n", "<leader>th", function()
-            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = bufnr }), { bufnr = bufnr })
-          end, "[T]oggle [H]int")
-          client.server_capabilities.semanticTokensProvider = nil
-        end
-      end
-
       vim.diagnostic.config({
         float = {
           border = "rounded",
@@ -50,7 +18,43 @@ return {
 
       vim.lsp.config('*', {
         capabilities = require("blink.cmp").get_lsp_capabilities(vim.lsp.protocol.make_client_capabilities()),
-        on_attach = on_lsp_attach,
+      })
+
+      -- Use LspAttach autocmd instead of on_attach in config
+      -- This ensures it fires for ALL LSP attachments, even if server configs have their own on_attach
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(args)
+          local bufnr = args.buf
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+
+          local Snacks = require("snacks")
+          local function map(mode, lhs, rhs, opts)
+            vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = "LSP " .. opts })
+          end
+
+          map("n", "K", function() vim.lsp.buf.hover({ border = "rounded" }) end, "Display hover info")
+          map("n", "gd", Snacks.picker.lsp_definitions, "[G]oto [D]efinition")
+          map("n", "gD", Snacks.picker.lsp_declarations, "[G]oto [D]eclaration")
+          map("n", "gi", Snacks.picker.lsp_implementations, "[G]oto [I]mplementation")
+          map("n", "gt", Snacks.picker.lsp_type_definitions, "[G]oto [T]ype definition")
+          map("n", "ga", vim.lsp.buf.code_action, "[G]et [A]ctions")
+          map("x", "ga", vim.lsp.buf.code_action, "[G]et [A]ctions")
+
+          map("n", "gl", vim.diagnostic.open_float, "Show diagnostics")
+
+          map("n", "<space>gr", vim.lsp.buf.rename, "[R]ename")
+          map("n", "<space>gd", Snacks.picker.diagnostics_buffer, "[G]get buffer [D]iagnostics")
+          map("n", "<space>gs", Snacks.picker.lsp_symbols, "[G]et [S]ymbols (document)")
+          map("n", "<space>gS", Snacks.picker.lsp_workspace_symbols, "[G]et [S]ymbols (workspace)")
+          map("n", "<space>gr", Snacks.picker.lsp_references, "[G]et [R]eferences")
+
+          if client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint, bufnr) then
+            map("n", "<leader>th", function()
+              vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = bufnr }), { bufnr = bufnr })
+            end, "[T]oggle [H]int")
+            client.server_capabilities.semanticTokensProvider = nil
+          end
+        end,
       })
 
       vim.lsp.set_log_level("off")
